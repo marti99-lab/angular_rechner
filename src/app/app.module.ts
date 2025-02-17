d153805b2f5e0405034f243f2919d48dee55b556
Martin <Martin@testmail.com>
2025-02-17 16:48:51 +0100

    change the working files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { UserComponent } from './user/user.component';
-import { TasksComponent } from './tasks/tasks.component';
-import { CardComponent } from './shared/card/card.component';
-import { TaskComponent } from './tasks/task/task.component';
-import { NewTaskComponent } from './tasks/new-task/new-task.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    UserComponent,
-    CardComponent,
-    TasksComponent,
-    TaskComponent,
-    NewTaskComponent,
+    HeaderComponent
   ],
   bootstrap: [AppComponent],
   imports: [BrowserModule, FormsModule],
